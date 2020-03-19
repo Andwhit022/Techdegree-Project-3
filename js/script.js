0e5abb2ba029117cbde88fb20559017e3fd4b330
Andwhit022 <59718154+Andwhit022@users.noreply.github.com>
2020-03-19 15:52:33 -0400

    create variables for user input

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//create variables for user input
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('mail');
+const cardNumber = document.getElementById('cc-num');
+const zipInput = document.getElementById('zip');
+const ccvInput = document.getElementById('ccv');
